5839143a5b04adaf4fdf985341560aa6db43e857
farheen931 <farheenrahim@live.co.uk>
2021-10-09 00:04:22 +0100

    runs now with old navbar from bootstap

--- capstone-project/src/components/Navbar.js
+++ capstone-project/src/components/Navbar.js
@@ -31,17 +31,20 @@ function Navbar() {
     return (
         <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container-fluid">
-          <a class="navbar-brand" href="www.google.com">Farheen Rahim</a>
+          <a class="navbar-brand" href="#">Something borrowed, something read</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="www.google.com">Home</a>
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#aboutme">About Me</a>
+                <a class="nav-link" href="#">About Me</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Me</a>
               </li>
             </ul>
           </div>
@@ -51,4 +54,4 @@ function Navbar() {
 }
 
 export default Navbar
-    
+
