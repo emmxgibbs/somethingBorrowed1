bb99285d87d8a92073915e02d603bd4a8df8c0bb
priya-ramesh-s <88005090+priya-ramesh-s@users.noreply.github.com>
2021-10-08 17:53:13 +0100

    commit2

---
+++ capstone-project/components/Homepage.js
@@ -0,0 +1,81 @@
+import React from 'react';
+// import Carousel from 'react-multi-carousel';
+// import 'react-multi-carousel/lib/styles.css';
+
+export default function Homepage() {
+    return(
+        <div className = "homepage-container">
+            <section>
+                <div className = "quotes">
+                    <img src = {} alt = "Image of a book" className = "quotes-image" />
+                    <p className = "book-quote"> Quote of the day...</p>
+                </div>        
+                <div className = "about-us">
+                    <div className = "row">
+                        <div className = "col-sm-4" align = "center">
+                            <Card imageSrc = {} />
+                        </div>
+                        <div className = "col-sm-4" align = "center">
+                            <Card imageSrc = {} />
+                        </div>
+                        <div className = "col-sm-4" align = "center">
+                            <Card imageSrc = {} />
+                        </div>
+                    </div>
+                </div>  
+                <div className = "fave-books">    </div>
+            </section>  
+        </div>                     
+    )
+
+    // Carousel feature
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3,
+        partialVisibilityGutter: 40 // this is needed to tell the amount of px that should be visible.
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        partialVisibilityGutter: 30 // this is needed to tell the amount of px that should be visible.
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        partialVisibilityGutter: 30 // this is needed to tell the amount of px that should be visible.
+      }
+    };
+{/* <Carousel partialVisible={true} responsive={responsive}>
+  <ItemOne />
+  <ItemTwo />
+</Carousel> */}
+
+{/* <Carousel
+  swipeable={false}
+  draggable={false}
+  showDots={true}
+  responsive={responsive}
+  ssr={true} // means to render carousel on server-side.
+  infinite={true}
+  autoPlay={this.props.deviceType !== "mobile" ? true : false}
+  autoPlaySpeed={1000}
+  keyBoardControl={true}
+  customTransition="all .5"
+  transitionDuration={500}
+  containerClass="carousel-container"
+  removeArrowOnDeviceType={["tablet", "mobile"]}
+  deviceType={this.props.deviceType}
+  dotListClass="custom-dot-list-style"
+  itemClass="carousel-item-padding-40-px"
+>
+  <div>Item 1</div>
+  <div>Item 2</div>
+  <div>Item 3</div>
+  <div>Item 4</div>
+</Carousel>; */}
+
+  // end of Carousel feature
+
+}
+
