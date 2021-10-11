fbaa9449384f1e66b48b88b1e37481de3c459993
priya-ramesh-s <88005090+priya-ramesh-s@users.noreply.github.com>
2021-10-11 15:21:17 +0100

    babel commit

--- capstone-project/src/components/Navbar.js
+++ capstone-project/src/components/Navbar.js
@@ -1,31 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-// function Navbar() {
-//     return (
-//         <><button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button><div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-//             <div class="offcanvas-header">
-//                 <h5 id="offcanvasRightLabel">Offcanvas right</h5>
-//                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-//             </div>
-//             <div class="offcanvas-body">
-//                 Text here
-//             </div>
-//         </div><div class="dropdown mt-3">
-//                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-//                     Dropdown button
-//                 </button>
-//                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-//                     <li><a class="dropdown-item" href="#">Action</a></li>
-//                     <li><a class="dropdown-item" href="#">Another action</a></li>
-//                     <li><a class="dropdown-item" href="#">Something else here</a></li>
-//                 </ul>
-//             </div></>
-//     )}
-
-// export default Navbar
-
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Navbar() {
