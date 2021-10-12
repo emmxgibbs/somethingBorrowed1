a17bcad200671ea92bd78c7ea702580b4187e787
priya-ramesh-s <88005090+priya-ramesh-s@users.noreply.github.com>
2021-10-12 20:28:46 +0100

    Update App.js

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -8,7 +8,7 @@ import Reviews from './components/Reviews';
 import SearchBar from './components/SearchBar';
 import { Route, Switch } from 'react-router-dom';
 // import '@fortawesome/fontawesome-free/css/all.min.css';
-// import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
 // import 'mdbreact/dist/css/mdb.css';
 
 function App() {
