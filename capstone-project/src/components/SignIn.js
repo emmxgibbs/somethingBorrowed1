823cde6c69053f4588307852881323ee0450420e
farheen931 <farheenrahim@live.co.uk>
2021-10-13 21:40:11 +0100

    sign in page commented out

--- capstone-project/src/components/SignIn.js
+++ capstone-project/src/components/SignIn.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import './SignIn.css'
-import { Link } from "react-router-dom";
-import Navbar from './Navbar';
+// import React from 'react'
+// import './SignIn.css'
+// import { Link } from "react-router-dom";
+// import Navbar from './Navbar';
 
 
-export default function SignIn(){
-    return(
-        <div className = "signin-container">
-          <Navbar/>
-        <form className="background">
-            <div className="container">
-                <h1> Sign in now! </h1>
-                <div className="label">
-                <label>
-                    Username: 
-                    <input type="text" name="username" /> 
-                </label>
-                <label>
-                    Password: 
-                    <input type="password" name="password" /> 
-                </label> 
-                </div> 
-                <input className= "submit"type="submit" value="Submit" />
-                <div className="Register"><h6><Link to="./register">Don't have an account? Register now</Link></h6></div>
-                </div>
-                </form>
-                </div>
-                    )
-                }
+// export default function SignIn(){
+//     return(
+//         <div className = "signin-container">
+//           <Navbar/>
+//         <form className="background">
+//             <div className="container">
+//                 <h1> Sign in now! </h1>
+//                 <div className="label">
+//                 <label>
+//                     Username: 
+//                     <input type="text" name="username" /> 
+//                 </label>
+//                 <label>
+//                     Password: 
+//                     <input type="password" name="password" /> 
+//                 </label> 
+//                 </div> 
+//                 <input className= "submit"type="submit" value="Submit" />
+//                 <div className="Register"><h6><Link to="./register">Don't have an account? Register now</Link></h6></div>
+//                 </div>
+//                 </form>
+//                 </div>
+//                     )
+//                 }
