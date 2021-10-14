6c3583ba581747f97fcf67bda78ca61a97689a62
emmxgibbs <88027784+emmxgibbs@users.noreply.github.com>
2021-10-14 16:15:54 +0100

    finished? who knows

--- src/main/java/com/User/MyUserDetailsService.java
+++ src/main/java/com/User/MyUserDetailsService.java
@@ -1,13 +1,14 @@
 package com.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
+
 
 @Service
 public class MyUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
