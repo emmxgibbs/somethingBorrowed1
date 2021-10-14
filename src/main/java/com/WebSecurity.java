6306bb0033f122bbc3722c0b1efee9b304d937ea
emmxgibbs <88027784+emmxgibbs@users.noreply.github.com>
2021-10-14 15:48:45 +0100

    finished? who knows

--- src/main/java/com/WebSecurity.java
+++ src/main/java/com/WebSecurity.java
@@ -1,55 +1,57 @@
 package com;
 
-import com.Authentication.AuthenticationRequestFilter;
-import com.User.MyUserDetailsService;
+import com.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.User.MyUserDetailsService;
 
-@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+@EnableJpaRepositories(basePackageClasses= UserRepository.class)
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter{
 
     @Autowired
-    private MyUserDetailsService myUserDetailsService;
+    MyUserDetailsService userDetailsService;
 
-    @Autowired
-    private AuthenticationRequestFilter jwtRequestFilter;
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService);
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(getPasswordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/html/**", "/images/**", "/css/**", "/js/**", "/font/**", "/img/**", "/ExerciseImg/**").permitAll()
-                .antMatchers("/authenticate", "/register", "/home", "/somethingBorrowed/submitLogin", "/search", "/trade", "/somethingBorrowed/book").permitAll()
-                .anyRequest().authenticated()
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("**/secure/**").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin().loginPage("/login").successForwardUrl("/rest/secured/loginSuccess").permitAll()
+                .and().logout().logoutSuccessUrl("/login?logout");
+
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private PasswordEncoder getPasswordEncoder(){
+        return new PasswordEncoder() {
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+            @Override
+            public boolean matches(CharSequence arg0, String arg1) {
+
+                return arg0.toString().equals(arg1);
+            }
+
+            @Override
+            public String encode(CharSequence charSeq) {
+
+                return charSeq.toString();
+            }
+        };
     }
 }
